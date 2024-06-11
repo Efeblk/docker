05e53ad8b2b4c7a7dab78fc52567f26de08254b4
efe dockervm <efe dockervm>
2024-06-11 13:12:50 -0400

    php8 but prob wrong

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+# Ensure necessary environment variables are set
+if [ -z "$WP_TITLE" ] || [ -z "$WP_ADMIN_LOGIN" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$WP_ADMIN_EMAIL" ] || [ -z "$WORDPRESS_DB_NAME" ] || [ -z "$WORDPRESS_DB_USER" ] || [ -z "$WORDPRESS_DB_PASSWORD" ] || [ -z "$WORDPRESS_DB_HOST" ]; then
+    echo "Missing one or more required environment variables."
+    exit 1
+fi
+
 # Check if WordPress is already installed
-if ! $(wp core is-installed --path=/var/www/html --allow-root); then
-    echo "Installing WordPress..."
-    wp core install --url="http://localhost" --title="Your Blog Title" --admin_user="admin" --admin_password="admin_password" --admin_email="you@example.com" --path=/var/www/html --allow-root
+if ! wp core is-installed --path=/var/www/html --allow-root; then
+    echo "Configuring WordPress..."
     wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --path=/var/www/html --allow-root
+    echo "Installing WordPress..."
+    wp core install --url="https://ibalik.42.fr" --title="$WP_TITLE" --admin_user="$WP_ADMIN_LOGIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path=/var/www/html --allow-root
 else
     echo "WordPress is already installed."
 fi
+
+# Ensure correct file permissions
+chown -R nginx:nginx /var/www/html
 
 # Start supervisord
 exec "$@"
